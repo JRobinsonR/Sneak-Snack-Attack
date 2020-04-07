6b787a1646ccd109b22d30ff94c26b44a8c4aafd
James Roach <jroach36@gmail.com>
2020-04-07 13:27:40 -0400

    added scene switch script

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    // scene management loads scene based on what you press for menu
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("YouLeft");
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
